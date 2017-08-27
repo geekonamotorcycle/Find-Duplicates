d0f9fd07b0fed1b4f4cb38707419d3766685e61d
Joshua Porrata <Joshua.porrata@gmail.com>
2017-08-26 22:05:25 -0400

    Version 1.0
    It works. 
    It is critical that you check the readme on this one.
    Enjoy.

--- Find-Duplicates.ps1
+++ Find-Duplicates.ps1
@@ -1,3 +1,21 @@
+# Find-duplicates 
+# Version 1.0
+# Designed for powershell 5.1
+# Copyright 2017 - Joshua Porrata
+# Not for business use without an inexpensive license, contact
+# Localbeautytampabay@gmail.com for questions about a lisence 
+# there is no warranty, this might destroy everything it touches. 
+
+#class HashHolder {
+#    [string]$FilePath
+#    [string]$FilePath2
+#    [string]$FileHash
+#    [string]$FileHash2
+#    [int]$Size
+#    [int]$Size2
+#    [datetime]$LastModified
+#    [datetime]$LastModified2
+#}
 class HashHolder {
     [string]$FilePath
     [string]$FilePath2
@@ -8,33 +26,174 @@ class HashHolder {
     [datetime]$LastModified
     [datetime]$LastModified2
 }
+Function Copyright {
+    Write-Host "`n***********************************************" -BackgroundColor Black -ForegroundColor DarkGreen
+    Write-Host "***Copyright 2017, Joshua Porrata**************" -BackgroundColor Black -ForegroundColor DarkGreen
+    Write-Host "***This program is not free for business use***" -BackgroundColor Black -ForegroundColor DarkGreen
+    Write-Host "***Contact me at localbeautytampabay@gmail.com*" -BackgroundColor Black -ForegroundColor DarkGreen
+    Write-Host "***for a cheap business license****************" -BackgroundColor Black -ForegroundColor DarkGreen
+    Write-Host "***Donations are wholeheartedly accepted ******" -BackgroundColor Black -ForegroundColor Red
+    Write-Host "***accepted @ www.paypal.me/lbtpa**************" -BackgroundColor Black -ForegroundColor Red
+    Write-Host "***********************************************`n" -BackgroundColor Black -ForegroundColor DarkGreen
+}
+
+#Read-paths 1.1
+#grabs the source and destination root Paths and checks that they 
+#exist before Passing them on.
+#<Switches>
+#	-Source will specify a source path Make sure its valid!
+#	-Destination will specify a destination path Make sure its valid!
+#	-findDestPath if $true, will ask for the destination Folder defualt $true
+#<Outputs>
+#	Output: $sourcePath the root of the folder we will be scanning.
+#	OutPut: $destPath The root of the folder files will be headed to 
+#Copyright 2017 Joshua Porrata, For private use only.
+Function Read-Paths {
+    Param
+    (
+        [Parameter(Mandatory = $False)]
+        [AllowEmptyString()]
+        $Source,
+        [Parameter(Mandatory = $False)]
+        [AllowEmptyString()]
+        $findDestPath = $true,
+        [Parameter(Mandatory = $False)]
+        [AllowEmptyString()]
+        $Destination
+    )
+ 
+
+    #Source logic
+    if ($source -ne $null) {
+        $testSource = Test-Path $Source
+        Write-Host "Source: $testsource`n"
+        $script:sourcePath = $Source
+    }
+    elseif ($source -eq $null) {
+        $i = 1
+        [boolean]$sourcePrompt = $true
+
+        while ($sourcePrompt) {
+            Write-Host "Enter Source Path [Attempt $i/3]" -ForegroundColor Green
+            $inputSource = Read-Host  -ErrorAction 'SilentlyContinue' -InformationAction 'SilentlyContinue'
+            $iSourceTest = Test-Path $inputSource 
+            if ($iSourceTest) {
+                Write-Host "The source path you entered Passed`n" -ForegroundColor Green
+                $script:sourcePath = $inputSource
+                $sourcePrompt = $False
+            }
+            else {
+                $i++        
+                Write-Host "The path could not be validated `n" -ForegroundColor Red
+            }
+            if ($i -gt 3) {
+                write-host "Too Many Attempts, Contact your Systems Adminstrator for help" -ForegroundColor Red
+                $script:runScript = $False
+                exit
+            }
+        }
+    }
+            
+    If ($findDestPath) { 
+        #Destination Logic
+        If ($destination -ne $null) {
+            $testdest = Test-Path $Destination
+            Write-Host "Destination: $testDest`n"
+            $script:destPath = $Destination
+        }
+        elseif ($Destination -eq $null) {
+            [boolean]$destPrompt = $true
+            $i = 1
+            while ($destPrompt) {
+                Write-Host "Enter Destination Path [Attempt $i/3]" -ForegroundColor Green
+                $inputDest = Read-Host 
+                $iDestTest = Test-Path -path $inputDest -ErrorAction 'SilentlyContinue'
+                if ($iDestTest) {
+                    Write-Host "The Destination Path you entered Passed`n" -ForegroundColor Green
+                    $script:destPath = $inputDest
+                    $destPrompt = $False
+                }
+                else {
+                    $i++        
+                    Write-Host "The path could not be validated `n" -ForegroundColor Red
+                }
+                if ($i -gt 3) {
+                    write-host "Too Many Attempts, Contact your Systems Adminstrator for help" -ForegroundColor Red
+                    $script:runScript = $False
+                    exit
+                }
+            }
+        }
+    }
+}
+
+#Get-HashObject 0.8
+#Recurses through a root folder, gathers file information and calculates SHA1 
+#Hash Values
+#<Switches>
+#	-SourcePath Sepcify a root path, this can be manually specified or sent from 
+#		$sourcePath from the Read-Paths Function. 
+#	-DumpCSV if $true a CSV of the output object wil be dropped into the same 
+#		Folder that the script was run from. Defaults $true
+#<Outputs>
+#	Output: $Script:LiveObject
+#	Output: .\File_Hash_results.csv
+# Copyright 2017 Joshua Porrata, For private use only.
 function Get-HashObject {
     param(
+        $DumpCSV = $true,    
         [AllowEmptyString()]    
         [String]$SourcePath
     )
+    class HashHolder {
+        [string]$FilePath
+        [string]$FilePath2
+        [string]$FileHash
+        [string]$FileHash2
+        [int]$Size
+        [int]$Size2
+        [datetime]$LastModified
+        [datetime]$LastModified2
+    }
 
     $startTime = [datetime]::Now
     $liveObject = @()
-    $infoObject = Get-ChildItem -File -Path "C:\users\joshp\downloads\" -Recurse
-    #$ObjectCount = $infoObject.count
-    #$i = 1
+    $infoObject = Get-ChildItem -File -Path $SourcePath -Recurse
+    Write-Host "`nScanning The folder below and generating Hashes`nthis may take some time `n$sourcePath `n" -ForegroundColor Green
     foreach ($file in $infoObject) {
         $loopHash = Get-FileHash -Path $file.FullName -Algorithm SHA1
         $loopHash = $loopHash.Hash
-        #$properties = @{FilePath = $file.FullName.ToString(); FileHash = $loopHash; Size = $file.Length; LastModified = $file.LastWriteTime}
         $loopObject = New-Object HashHolder -Property @{FilePath = $file.FullName.ToString(); FileHash = $loopHash; Size = $file.Length; LastModified = $file.LastWriteTime} #$properties 
         $liveObject += $loopObject
-        #Write-Progress -Activity "Generating File Hashes" -Status "Generating" -PercentComplete ($i / $ObjectCount * 100) -CurrentOperation "$i / $objectCount $loopHash $file"
-        #$i++
     }
     $endtime = [datetime]::Now
     $runtime = ($startTime - $endtime)
-    Write-Host $runtime
-    $liveObject | ConvertTo-Csv -NoTypeInformation > File_Hash_results.csv    
+    Write-Host "`nHash Generation took: " -ForegroundColor Green
+    Write-Host $runtime 
+    $Script:LiveObject = $liveObject 
+    if ($DumpCSV) {
+        $liveObject | Export-Csv -Encoding UTF8 -NoTypeInformation -path .\File_Hash_results.csv
+    } 
 }
+
+#Remove-ZeroLength 0.8
+#Iterates through the input array and seperates files of less than a specified 
+#Length out and into a seperate array and then dumps them into a CSV file. 
+#<Switches>
+#	-LengthFilter, Minimum length a file must be to pass through the seperation
+#		Default 1
+#	-ArrayIn, the input array, in this script the array comes from 
+#		Get-HashObject and is named $liveObject
+#	-DumpCSV if $true a CSV of the output object wil be dropped into the same 
+#		Folder that the script was run from. Defaults $true
+#<Output>
+#	Output: $PenultimateTable
+#	Output: .\pFile_Hash_results.csv
+#	Output: .\zFile_Hash_results.csv
+# Copyright 2017 Joshua Porrata, For private use only.
 function Remove-ZeroLength {
     param(
+        $DumpCSV = $true,
         [int]$LengthFilter = 1,
         $ArrayIn
     )
@@ -50,9 +209,9 @@ function Remove-ZeroLength {
     }
     $zLengthArray = @()
     $pLengthArray = @()
-    $ArrayIn = Import-Csv -Path .\File_Hash_results.csv
-    if ($ArrayIn -ne $null) {
-        Write-Host "Found Input Object, Scanning for Zero File Size"
+    if ($ArrayIn -eq $null) {
+        Write-Host "`nInput array is empty Attempting to read 'File_Hash_results.csv', Scanning for Zero File Size`n" -ForegroundColor Yellow
+        $ArrayIn = Import-Csv -Path .\File_Hash_results.csv
         foreach ($line in $ArrayIn) {
             if ($line.size -le $LengthFilter) {
                 $properties = @{Filepath = $line.FilePath; FileHash = $line.FileHash; Size = $line.size}
@@ -65,25 +224,86 @@ function Remove-ZeroLength {
                 $pLengthArray += $pLength
             }
         }
-        $pLengthArray | Export-Csv -Encoding UTF8 -NoTypeInformation -Path pFile_Hash_results.csv
-        $zLengthArray | Export-Csv -Encoding UTF8 -NoTypeInformation -Path Zero_Length_Results.csv
+        $pLengthArray | Export-Csv -Encoding UTF8 -NoTypeInformation -Path .\pFile_Hash_results.csv
+        $zLengthArray | Export-Csv -Encoding UTF8 -NoTypeInformation -Path .\Zero_Length_Results.csv
     }
+    else {
+        Write-Host "`nFound Input Array, Scanning for Zero File Size`n" -ForegroundColor Green
+        foreach ($line in $ArrayIn) {
+            if ($line.size -le $LengthFilter) {
+                $properties = @{Filepath = $line.FilePath; FileHash = $line.FileHash; Size = $line.size}
+                $zLength = New-Object -TypeName HashHolder -Property $properties
+                $zLengthArray += $zLength
+            }
+            else {
+                $properties = @{FilePath = $line.FilePath; FileHash = $line.FileHash; Size = $line.size; LastModified = $line.LastModified}
+                $pLength = New-Object -TypeName HashHolder -Property $properties
+                $pLengthArray += $pLength
+            }
+        }
+        if ($dumpCSV) {
+            $pLengthArray | Export-Csv -Encoding UTF8 -NoTypeInformation -Path .\pFile_Hash_results.csv
+            $zLengthArray | Export-Csv -Encoding UTF8 -NoTypeInformation -Path .\Zero_Length_Results.csv
+        }
+    }
+    $script:PenultimateTable = $pLengthArray
 }
+
+#Find-HashMatch 0.8
+#Makes two copies of the Hahsholder Obect in memory and iterates through them 
+#using brute force to find if there is more than one match to a hash. A CSV is 
+#written to disk of all matching Hashes 
+#<Switches>
+#	-DumpCSV if $true a CSV of the output object wil be dropped into the same 
+#		Folder that the script was run from. Defaults $true
+#	-InputArray The input array to be processed. This comes from 
+#		Remove-ZeroLength and is named $PenultimateTable or, if null it will try
+#		to find .\pFile_Hash_results.csv and import that.
+#	-DisplayOutput, sends the final array to terminal with out-gridview. Be 
+#		careful. defaults true.
+#<Output>
+#	Output: .\Hash_Match_list.csv
+#	Output: Out-GridArray to terminal 
+# Copyright 2017 Joshua Porrata, For private use only.
 Function Find-HashMatch {
+    Param(
+        $DisplayOutput = $true,    
+        $DumpCSV = $true,
+        $InputArray
+    )
+    class HashHolder {
+        [string]$FilePath
+        [string]$FilePath2
+        [string]$FileHash
+        [string]$FileHash2
+        [int]$Size
+        [int]$Size2
+        [datetime]$LastModified
+        [datetime]$LastModified2
+    }
     $startTime = [datetime]::Now
     $liveObject = @()
-    if (Test-Path .\pFile_Hash_results.csv) {
-        Write-Host "You are in the right place..."
-        $object1 = Import-Csv -Path .\pFile_Hash_results.csv | Sort-Object -Descending
+    if ($inputArray -eq $null ) {
+        if (Test-Path .\pFile_Hash_results.csv) {
+            Write-Host "`nThe expected input array was null and could not be processed. " -ForegroundColor Yellow
+            Write-Host "Attempting to import .\pFile_Hash_results.csv `n" -ForegroundColor Yellow
+            $inputArray = Import-Csv -Path .\pFile_Hash_results.csv | Sort-Object -Descending
+        }
+        else {
+            Write-Host "`nThe expected input array was null and could not be processed. " -ForegroundColor Red
+            Write-Host "Attempted to import .\pFile_Hash_results.csv but failed `n" -ForegroundColor Red
+        }
+       
+    }
+    else {
+        Write-Host "Making Comparisons, Attempting to find matches... This will take some time `n" -ForegroundColor Yellow
+        $object1 = $inputArray
         $object2 = $object1
-        #$objectCount = $object1.count
-        #$i = 1
         $MatchCount = 0
         foreach ($Hashline in $object2) {
             foreach ($line1 in $object1) {
                 $test = $line1.FileHash.equals($Hashline.FileHash)
                 if ($test) {
-                    #Write-Host "Match Found!" $line1.FullName
                     $MatchCount++
                     if ($MatchCount -gt 2) {
                         $properties = @{FilePath = $HashLine.FilePath; FileHash = $HashLine.FileHash; Size = $Hashline.size; LastModified = $Hashline.LastModified}
@@ -91,22 +311,39 @@ Function Find-HashMatch {
                         $foundMatches = New-Object HashHolder -Property $properties
                         $liveObject += $foundMatches
                     }
-                    #Write-Progress -Activity "Iterating" -Status "Work Work Work, all day long" -PercentComplete ($i / $objectCount * 100) -CurrentOperation "$i / $objectCount"
-                    #$i++
                 }
-            }$MatchCount = 0
-        }
+            }
 
-        $liveObject | Out-GridView -Title "Matches"
-        $liveObject | ConvertTo-Csv -NoTypeInformation > Hash_Match_list.csv
-    }
-    else {
-        Get-HashObject
+        }
+        $MatchCount = 0
+        if ($DisplayOutput) {   
+            $liveObject | Out-GridView -Title "Matches"
+        }
+        if ($DumpCSV) {
+            $liveObject | Export-Csv -Encoding UTF8 -NoTypeInformation -path .\Hash_Match_list.csv
+        }
     }
     $endtime = [datetime]::Now
     $runtime = ($startTime - $endtime)
+    Write-Host "Matchmaking running time was:" -ForegroundColor Green
     Write-Host $runtime
+    Write-Host ""
 }
-#Get-HashObject 
-Remove-ZeroLength
-Find-HashMatch
+
+Clear-Host
+Copyright
+Read-Paths -findDestPath $false 
+Get-HashObject -SourcePath $sourcePath -DumpCSV $true
+Remove-ZeroLength -ArrayIn $liveObject -LengthFilter 1 -DumpCSV $true
+Find-HashMatch -InputArray $PenultimateTable -DumpCSV $true -DisplayOutput $
+
+<#
+Default Execution Order.
+
+Clear-Host
+Copyright
+Read-Paths -findDestPath $false  
+Get-HashObject -SourcePath $sourcePath -DumpCSV $true
+Remove-ZeroLength -ArrayIn $liveObject -LengthFilter 1 -DumpCSV $true
+Find-HashMatch -InputArray $PenultimateTable -DumpCSV $true -DisplayOutput $true
+#>
--- README.md
+++ README.md
@@ -1,2 +1,125 @@
-# Find-Duplicates
-Grr
+# Find-Duplicates.ps1 #
+### Copyright 2017 Joshua Porrata<br>For private use only. For business use please contact localbeautytampabay@gmail.com for a low cost <br>license. Honor system rules###
+----------
+
+* **Why?**<br> 
+Because I have a lot of duplicate and 0 byte images and I would like to know where they are so I can remove them.
+
+* **How do I run this script?**
+	*	run  `.\Find-Duplicates.ps1`
+* **What are the defaults?**
+	1. The script will ask for a source path
+
+	2. The script will dump a CSV file of its initial scan. the name is `File_Hash_results.csv`. The initial scan can be lengthy so this file is dumped in case you need to pick up later.
+
+	3. Two more CSV files are dumped: 
+		1. `pFile_Hash_results.csv` (p) stands for positive and contains files with a greater than zero length. 
+		2. `zFile_Hash_results.csv` (z) if zero length, these are essentially empty files.  <br>
+
+	4. Finally a CSV named `Hash_Match_List.csv` and an `Out-Gridview` window will be shown.
+
+* **How does it work?**<br>The script is split into several functions
+
+	1. **Read-Paths**: asks for a source path and tests it
+
+	2. **Get-HashObjects**: recurses through the source path and generates SHA1 Hashes.
+
+	3. **Remove-ZeroLength**: increments goes through the results of <br>  Get-HashObjects and creates two CSV files, one of zero length files and one of positive length files.
+
+	4. **Find-Hashmatch**: Brute force; Grabs the hash of the first line in the output and compares it to every other hash, when it finds 2 (1 match is always expected) or more matches it logs them and dumps out a CSV with the results.
+
+* **Will this run on OSX, Linux, or Powershell `(version -ne 5.1)`**
+	
+	* I have no idea
+
+* **It broke my computer,deleted files, exploded!**
+	* There is no warranty or guarantee that this script will work the way you or I think 	it will. Good Luck.
+*	**This seems terribly inefficient.**
+	*	I'm no mathematician, but that sounds right. 
+
+----------
+#Functions, Switches, Inputs, Outputs, Class, Executions Order
+
+```
+#Read-paths 1.1
+#grabs the source and destination root Paths and checks that they 
+#exist before Passing them on.
+#<Switches>
+#	-Source will specify a source path Make sure its valid!
+#	-Destination will specify a destination path Make sure its valid!
+#	-findDestPath if $true, will ask for the destination Folder defualt $true
+#<Outputs>
+#	Output: $sourcePath the root of the folder we will be scanning.
+#	OutPut: $destPath The root of the folder files will be headed to 
+#Copyright 2017 Joshua Porrata, For private use only.
+
+
+#Get-HashObject 0.8
+#Recurses through a root folder, gathers file information and calculates SHA1 
+#Hash Values
+#<Switches>
+#	-SourcePath Sepcify a root path, this can be manually specified or sent from 
+#		$sourcePath from the Read-Paths Function. 
+#	-DumpCSV if $true a CSV of the output object wil be dropped into the same 
+#		Folder that the script was run from. Defaults $true
+#<Outputs>
+#	Output: $Script:LiveObject
+#	Output: .\File_Hash_results.csv
+# Copyright 2017 Joshua Porrata, For private use only.
+
+#Remove-ZeroLength 0.8
+#Iterates through the input array and seperates files of less than a specified 
+#Length out and into a seperate array and then dumps them into a CSV file. 
+#<Switches>
+#	-LengthFilter, Minimum length a file must be to pass through the seperation
+#		Default 1
+#	-ArrayIn, the input array, in this script the array comes from 
+#		Get-HashObject and is named $liveObject
+#	-DumpCSV if $true a CSV of the output object wil be dropped into the same 
+#		Folder that the script was run from. Defaults $true
+#<Output>
+#	Output: $PenultimateTable
+#	Output: .\pFile_Hash_results.csv
+#	Output: .\zFile_Hash_results.csv
+# Copyright 2017 Joshua Porrata, For private use only.
+
+#Find-HashMatch 0.8
+#Makes two copies of the Hahsholder Obect in memory and iterates through them 
+#using brute force to find if there is more than one match to a hash. A CSV is 
+#written to disk of all matching Hashes 
+#<Switches>
+#	-DumpCSV if $true a CSV of the output object wil be dropped into the same 
+#		Folder that the script was run from. Defaults $true
+#	-InputArray The input array to be processed. This comes from 
+#		Remove-ZeroLength and is named $PenultimateTable or, if null it will try
+#		to find .\pFile_Hash_results.csv and import that.
+#	-DisplayOutput, sends the final array to terminal with out-gridview. Be 
+#		careful. defaults true.
+#<Output>
+#	Output: .\Hash_Match_list.csv
+#	Output: Out-GridArray to terminal 
+# Copyright 2017 Joshua Porrata, For private use only.
+
+#class HashHolder {
+#    [string]$FilePath
+#    [string]$FilePath2
+#    [string]$FileHash
+#    [string]$FileHash2
+#    [int]$Size
+#    [int]$Size2
+#    [datetime]$LastModified
+#    [datetime]$LastModified2
+#}
+
+Default Execution Order.
+
+Clear-Host
+Copyright
+Read-Paths -findDestPath $false
+Get-HashObject -SourcePath $sourcePath -DumpCSV $true
+Remove-ZeroLength -ArrayIn $liveObject -LengthFilter 1 -DumpCSV $true
+Find-HashMatch -InputArray $PenultimateTable -DumpCSV $true -DisplayOutput $true
+
+ ```
+
+
